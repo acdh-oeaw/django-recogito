3961c1c29b5dcf31daa3a9e5af01638caacc1408
csae8092 <peter.andorfer@oeaw.ac.at>
2021-05-18 12:31:48 +0200

    basic test for example-app added

--- example/tests.py
+++ example/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from example.models import MyText
 
-# Create your tests here.
+
+class ExampleTestCase(TestCase):
+    fixtures = ['fixtures.json']
+
+    def setUp(self):
+        # Test definitions as before.
+        pass
+
+    def test_001_model_count(self):
+        # A test that uses the fixtures.
+        items = MyText.objects.all()
+        self.assertTrue(items.count(), 1)
+    
+    def test_002_details(self):
+        response = self.client.get('/my-text/1/')
+        self.assertEqual(response.status_code, 200)
